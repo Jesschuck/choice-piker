c2b01b317622247dec6c10532d7a0b609149909a
Jesschuck <jaayschuck@gmail.com>
2024-01-26 13:23:16 -0300

    Description of functions in the script

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const tagsEl = document.getElementById('tags')
 const textarea = document.getElementById('textarea')
 
+//O cursor é colocado no textarea assim que a página é carregada.
 textarea.focus()
 
+/* addEventListener é adicionado ao textarea para o evento de tecla liberada (keyup). 
+Quando uma tecla é liberada, a função anônima é chamada, que realiza as seguintes ações:
+- Chama a função createTags passando o valor atual do textarea.
+- Se a tecla pressionada for 'Enter', limpa o textarea após 10 milissegundos (usando setTimeout) e chama a função randomSelect. */
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
 
@@ -15,6 +20,9 @@ textarea.addEventListener('keyup', (e) => {
   }
 })
 
+/*Recebe um input (string), que contém tags separadas por vírgulas. 
+Remove espaços em branco desnecessários e cria elementos span para cada tag. 
+Esses elementos span são adicionados ao elemento com o ID 'tags' no HTML. */
 function createTags(input) {
   const tags = input
     .split(',')
@@ -31,6 +39,9 @@ function createTags(input) {
   })
 }
 
+/*Realiza uma animação de destaque aleatório entre as tags. 
+Durante um intervalo de tempo, escolhe aleatoriamente uma tag, a destaca e, após 100 milissegundos, remove o destaque. 
+Este processo é repetido várias vezes */
 function randomSelect() {
   const times = 30
 
@@ -55,11 +66,13 @@ function randomSelect() {
   }, times * 100)
 }
 
+//Retorna uma tag HTML aleatória do conjunto de tags existentes.
 function pickRandomTag() {
   const tags = document.querySelectorAll('.tag')
   return tags[Math.floor(Math.random() * tags.length)]
 }
 
+//Adiciona ou remove a classe 'highlight' de uma tag específica que é usada para dar o destaque visual à tag escolhida.
 function highlightTag(tag) {
   tag.classList.add('highlight')
 }
